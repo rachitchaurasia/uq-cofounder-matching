10e99f0a748858fbd46a74459650991198aa179c
7t1anw <7t1wang@gmail.com>
2025-05-08 08:29:00 +1000

    conversation model implementation, NOT yet migration

--- matchingapp-backend/matchingapp/chat/models.py
+++ matchingapp-backend/matchingapp/chat/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+class Conversation(models.Model):
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='conversations',
+        verbose_name="participants"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created at"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Last active time"
+    )
+    
+    class Meta:
+        verbose_name = "Conversation"
+        verbose_name_plural = "Conversation"
+    
+    def __str__(self):
+        return f"Conversation {self.id}"
